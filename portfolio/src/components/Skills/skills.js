1df28dfe3f410ee5d254b2c1399716be932e234f
alicia <a.mao@share.epsb.ca>
2024-12-30 11:33:28 -0700

    finished skills section and started portfolio section

---
+++ portfolio/src/components/Skills/skills.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './skills.css';
+import SkillsImg from '../../assets/skills.png';
+import ToolsImg from '../../assets/tools.png';
+import InterestsImg from '../../assets/interests.png';
+
+const Skills = () => {
+    return (
+        <section id='skills'>
+            <span className='skillTitle'>What I do</span>
+            <span className='skillDescription'>I am a Software Engineering student at the University of Waterloo skilled in both front-end and back-end development. I enjoy the process of creating something new, whether that be a website, game, or something else entirely. </span>
+            <div className="skillBars">
+                <div className="skillBar">
+                    <img src={SkillsImg} alt = "SkillsImg" className="skillBarImg"></img>
+                    <div className="skillBarText">
+                        <h2>Skills</h2>
+                        <p>Python, C, C++, SQL, sqlite3, HTML, CSS, Javascript</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={ToolsImg} alt = "ToolsImg" className="skillBarImg"></img>
+                    <div className="skillBarText">
+                        <h2>Tools</h2>
+                        <p>VSCode, PuTTY, PyCharm, MySQL, React, Flask, Object Oriented Programming</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={InterestsImg} alt = "InterestsImg" className="skillBarImg"></img>
+                    <div className="skillBarText">
+                        <h2>Interests</h2>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Skills
